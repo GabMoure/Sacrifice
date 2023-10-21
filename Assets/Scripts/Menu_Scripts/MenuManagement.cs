99e73c969c31b22004a787e95b55b1df36e953ad
JBPX <gabrielsantosdemoura93@gmail.com>
2023-10-21 16:12:05 -0300

    Volume e Graficos

---
+++ Assets/Scripts/Menu_Scripts/MenuManagement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManagement : MonoBehaviour
+{
+
+    public void load()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // proxima cena na fila
+    }
+    public void quit()
+    {
+        Debug.Log("Saiu");
+        Application.Quit();
+    }
+}
