fb56506ee6786ab74bdef675e3da0c4ae31656f0
JBPX <gabrielsantosdemoura93@gmail.com>
2023-10-21 17:08:55 -0300

    Mudando resoluções
    ainda falta:
    - Trocar fonte para uma mais bonita
    - distribuir melhor as opções no menu
    - fundo no menu

--- Assets/Scripts/Menu_Scripts/Options_Manager.cs
+++ Assets/Scripts/Menu_Scripts/Options_Manager.cs
@@ -2,10 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class Options_Manager : MonoBehaviour
 {
+    public TMP_Dropdown resolutiondropdown;
     public AudioMixer audioMixer;
+    Resolution[] resolutions; //array com todas as resoluções
+    void Start()
+    {
+        //colocando as resoluções na unity dentro do array
+        resolutions = Screen.resolutions;
+        //limpando as opções prévias do dropdown
+        resolutiondropdown.ClearOptions();
+        //criando lista
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        //adicionando as strings da lista nas opções do dropdown
+        resolutiondropdown.AddOptions(options);
+        resolutiondropdown.value = currentResolutionIndex;
+        resolutiondropdown.RefreshShownValue();
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -14,5 +44,16 @@ public class Options_Manager : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionindex)
+    {
+        Resolution resolution = resolutions[resolutionindex]; 
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); 
     }
 }
