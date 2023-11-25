e6ca96d84a69322bc828fbf00a769586da624ab4
JBPX <gabrielsantosdemoura93@gmail.com>
2023-11-25 15:46:25 -0300

    Básico do Monstro
    Comportamento básico do monstro:
    - Caminhar entre localizações de forma aleatória
    - Perseguir player quando o mesmo esta em vista
    - Matar o player em contato
    ------
    Falta:
    
    - Teleportar para localizações específicas em cenas da história(quando o player pega um item, entra em uma área etc.)
    - Sistema de esconder

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyScript : MonoBehaviour
 {
@@ -9,9 +10,11 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] List<Transform> destinations;
     [SerializeField] List<Transform> CanonEvents;
     [SerializeField] Animator anim;
-    [SerializeField] float Walkspeed,Runspeed, minIdleTime, maxIdleTime, IdleTime;
+    [SerializeField] float Walkspeed,Runspeed, minIdleTime, maxIdleTime, IdleTime, raycastDistance, 
+    minChaseTime, maxChaseTime, ChaseTime, jumpscareTime;
     [SerializeField] Transform Player;
     [SerializeField] int DestinationAmount;
+    [SerializeField] GameObject PlayerCamera;
     private Vector3 Direction;
     private int randNum, randNum2;
     private Transform currentDestination;
@@ -20,19 +23,38 @@ public class EnemyScript : MonoBehaviour
     void Start()
     {
         walking = true;
+        chasing = false;
         randNum = Random.Range(0, DestinationAmount);
         currentDestination = destinations[randNum];
     }
     void Update()
     {
-        Vector3
+        Vector3 towardsDirection = (Player.position - transform.position).normalized;
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, towardsDirection * 15);
+        if (Physics.Raycast(transform.position, towardsDirection, out hit, raycastDistance))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                walking = false;
+                StopCoroutine(StayIdle());
+                StopCoroutine(ChaseRoutine());
+                StartCoroutine(ChaseRoutine());
+                //setting run trigger
+                anim.ResetTrigger("idle");
+                anim.ResetTrigger("walk");
+                anim.SetTrigger("chase");
+                //boolean
+                chasing = true;
+            }
+        }
+
         if (walking == true)
         {
-            anim.ResetTrigger("idle");
-            anim.SetTrigger("walk");
             Direction = currentDestination.position;
             ai.destination = Direction;
             ai.speed = Walkspeed;
+            anim.SetTrigger("walk");
             if (ai.remainingDistance <= ai.stoppingDistance)
             {
                 randNum2 = Random.Range(0,1);
@@ -40,7 +62,6 @@ public class EnemyScript : MonoBehaviour
                 {
                     randNum = Random.Range(0, DestinationAmount);
                     currentDestination = destinations[randNum];
-
                 }
                 if (randNum == 1)
                 {
@@ -50,6 +71,14 @@ public class EnemyScript : MonoBehaviour
                     walking = false;
                 }
             }
+            ai.updateRotation = true;
+        }
+        
+        if (chasing == true)
+        {
+            Direction = Player.position;
+            ai.destination = Direction;
+            ai.speed = Runspeed;
             ai.updateRotation = true;
         }
     }
@@ -64,5 +93,36 @@ public class EnemyScript : MonoBehaviour
         anim.SetTrigger("walk");
         walking = true;
         ai.updateRotation = true;
+    }
+
+    IEnumerator ChaseRoutine()
+    {
+        ChaseTime = Random.Range(minChaseTime, maxChaseTime);
+        yield return new WaitForSeconds(ChaseTime);
+        randNum = Random.Range(0, DestinationAmount);
+        currentDestination = destinations[randNum];
+        anim.ResetTrigger("chase");
+        anim.SetTrigger("walk");
+        chasing = false;
+        walking = true;
+        ai.updateRotation = true;
+    }
+    IEnumerator KillPlayer()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(chasing == true && collider.gameObject.tag == "Player")
+        {
+            anim.ResetTrigger("chase");
+            PlayerCamera.SetActive(false);
+            anim.SetTrigger("kill");
+            StartCoroutine(KillPlayer());
+            chasing = false;
+        }
     }
 }
